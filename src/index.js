8440183a1cf5df20084fc21e95e1b237143dd719
Rohit Sonawane <rrsonawne@gmail.com>
2022-07-25 11:33:46 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./routes/route');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Working')
+})
+
+mongoose
+  .connect(
+    'mongodb+srv://rohit_sonawane:SuperSu@cluster0.e9hjfiy.mongodb.net/group31Database'
+  )
+  .then(() => console.log('MongoDb is connected'))
+  .catch(err => console.log(err));
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log(`Express app running on port ${process.env.PORT || 3000}`);
+});
