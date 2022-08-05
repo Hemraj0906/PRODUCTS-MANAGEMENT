3fb21c9ea66b801bf3e4ba101a4cf252b66906cf
Rohit Sonawane <rrsonawne@gmail.com>
2022-08-05 23:35:13 +0530

    update

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -1,9 +1,9 @@
-const orderModel = require("../model/orderModel");
-const cartModel = require("../model/cartModel");
-const userModel = require("../model/userModel");
-const { isValid } = require("../validations/validations");
+const orderModel = require('../model/orderModel');
+const cartModel = require('../model/cartModel');
+const userModel = require('../model/userModel');
+const { isValid } = require('../validations/validations');
 
-const { isValidObjectId } = require("mongoose");
+const { isValidObjectId } = require('mongoose');
 
 const createOrder = async function (req, res) {
   try {
@@ -13,24 +13,24 @@ const createOrder = async function (req, res) {
     if (!Object.keys(req.body))
       return res
         .status(400)
-        .send({ status: false, message: "body cannot be empty" });
+        .send({ status: false, message: 'body cannot be empty' });
     if (!isValidObjectId(userId))
       return res
         .status(400)
-        .send({ status: false, message: "userId is invalid" });
+        .send({ status: false, message: 'userId is invalid' });
     const user = await userModel.findOne({ _id: userId });
     if (!user)
       return res
         .status(400)
-        .send({ status: false, message: "userId does not exist" });
+        .send({ status: false, message: 'userId does not exist' });
     if (!isValid(cartId))
       return res
         .status(400)
-        .send({ status: false, message: "cartId cannot be empty" });
+        .send({ status: false, message: 'cartId cannot be empty' });
     if (!isValidObjectId(cartId))
       return res
         .status(400)
-        .send({ status: false, message: "cartId is invalid" });
+        .send({ status: false, message: 'cartId is invalid' });
     const cart = await cartModel.findOne({
       _id: cartId,
       isDeleted: false,
@@ -38,21 +38,21 @@ const createOrder = async function (req, res) {
     if (cart && cart?.userId?.toString() !== userId)
       return res
         .status(400)
-        .send({ status: false, message: "cart does not exist on this user" });
+        .send({ status: false, message: 'cart does not exist on this user' });
     if (!cart)
       return res
         .status(400)
-        .send({ status: false, message: "cart does not exist " });
+        .send({ status: false, message: 'cart does not exist ' });
 
     if (cancellable) {
       if (!isValid(cancellable))
         return res
           .status(400)
-          .send({ status: false, message: "cancellable cannot be empty" });
+          .send({ status: false, message: 'cancellable cannot be empty' });
       if (!(cancellable == false || cancellable == true))
         return res
           .status(400)
-          .send({ status: false, message: "cancellable must be a boolean" });
+          .send({ status: false, message: 'cancellable must be a boolean' });
     }
     let totalQuantity = 0;
     const cartItems = cart.items;
@@ -74,32 +74,32 @@ const createOrder = async function (req, res) {
 
 const updateOrder = async function (req, res) {
   try {
-    const {userId} = req.params;
+    const { userId } = req.params;
     const { orderId } = req.body;
 
     //Validations
     if (!isValid(orderId)) {
       return res
         .status(400)
-        .send({ status: false, message: "Please enter orderId" });
+        .send({ status: false, message: 'Please enter orderId' });
     }
 
     if (!isValidObjectId(userId)) {
       return res
         .status(400)
-        .send({ status: false, message: "userId is invalid" });
+        .send({ status: false, message: 'userId is invalid' });
     }
     if (!isValidObjectId(orderId)) {
       return res
         .status(400)
-        .send({ status: false, message: "orderId is invalid" });
+        .send({ status: false, message: 'orderId is invalid' });
     }
 
     const isUserExists = await userModel.findById(userId);
     if (!isUserExists) {
       return res
         .status(404)
-        .send({ status: false, message: "userData not found" });
+        .send({ status: false, message: 'userData not found' });
     }
 
     const isOrderExists = await orderModel.findOne({
@@ -109,10 +109,14 @@ const updateOrder = async function (req, res) {
     if (!isOrderExists) {
       return res
         .status(404)
-        .send({ status: false, message: "orderData not found" });
+        .send({ status: false, message: 'orderData not found' });
     }
     //If the cancellable is false then order can't be cancelled.
-    if (isOrderExists.cancellable == false)
+    if (isOrderExists.status == 'cancelled')
+      return res
+        .status(400)
+        .send({ status: false, message: 'This order is already cancelled' });
+    if (isOrderExists.cancellable != true)
       return res
         .status(400)
         .send({ status: false, message: "This order can't be cancelled" });
@@ -120,24 +124,24 @@ const updateOrder = async function (req, res) {
     if (isOrderExists.userId != userId) {
       return res
         .status(400)
-        .send({ status: false, message: "order not belongs to the user" });
+        .send({ status: false, message: 'order not belongs to the user' });
     }
 
     const updatedData = await orderModel.findOneAndUpdate(
       { _id: orderId, isDeleted: false },
-      { status: "cancled", isDeleted: true, deletedAt: Date.now() },
+      { status: 'cancelled' },
       { new: true }
     );
 
     if (!updatedData) {
       return res
         .status(404)
-        .send({ status: false, message: "data not found for update" });
+        .send({ status: false, message: 'data not found for update' });
     }
 
     return res.status(200).send({
       status: true,
-      message: "order Cancelled successfully",
+      message: 'order Cancelled successfully',
       data: updatedData,
     });
   } catch (error) {
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,65 +1,66 @@
-const jwt = require("jsonwebtoken");
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const jwt = require('jsonwebtoken');
 
-const userModel=require('../model/userModel')
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
+const userModel = require('../model/userModel');
+
 exports.authentication = async function (req, res, next) {
   try {
-    let userId = req.params.userId;
+    const { userId } = req.params;
      // if userId is not a valid ObjectId
     if (!mongoose.isValidObjectId(userId)) {
       return res.status(400).send({
         status: false,
-        message: "userId is invalid",
+        message: 'userId is invalid'
       });
     }
     let token = req.headers.authorization;
     if (!token) {
       return res
       .status(400)
-      .send({ status: false, message: "Token must be present" });
+        .send({ status: false, message: 'Token must be present' });
     }
-    token = token.split(" ")[1]
+    token = token.split(' ')[1];
 
-    const decodedToken = jwt.verify(
-      token,
-      "functionup-radon",
-      function (err, decodedToken) {
-        if (err) return null;
-        return decodedToken;
-      }
-    );
+    const decodedToken = jwt.verify(token, 'functionup-radon', function(
+      err,
+
+      tokeneed
+    ) {
+      if (err) return null;
+      return tokeneed;
+    });
     if (!decodedToken)
       return res
         .status(400)
-        .send({ status: false, message: "Token is invalid" });
-        req.key= token
+        .send({ status: false, message: 'Token is invalid' });
+    req.key = token;
     next();
   } catch (err) {
     res.status(500).send({ status: false, message: err.message });
   }
 };
 
-exports.authorization =async function (req, res, next) {
-  let token = req.key
+exports.authorization = async function(req, res, next) {
+  const token = req.key;
 
   //If no token is present in the request header return error
   if (!token)
     return res
       .status(400)
-      .send({ status: false, msg: "token must be present" });
-  const decodedtoken = jwt.verify(token, "functionup-radon");
+      .send({ status: false, msg: 'token must be present' });
+  const decodedtoken = jwt.verify(token, 'functionup-radon');
   const { userId } = req.params;
-  const searchForId= await userModel.findById(userId)
-  if(!searchForId) return res
-  .status(400)
-  .send({ status: false, msg: "invalid id" });
+  const searchForId = await userModel.findById(userId);
+  if (!searchForId)
+    return res.status(400).send({ status: false, msg: 'invalid id' });
   if (decodedtoken.userId == userId) {
-    next();
+    next()
 
   } else {
     return res.status(403).send({
       status: false,
-      msg: "The Login User Are not authorize to do this actions",
+      msg: 'The Login User Are not authorize to do this actions'
     });
   }
 };
